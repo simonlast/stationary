0819dd73431fe28f4ea1d22a68ec1e98a949b005
Simon Last <stlast1@gmail.com>
2013-12-07 21:51:59 -0800

    turn off editor.

--- server/files/main.js
+++ server/files/main.js
@@ -1,6 +1,6 @@
 window.debug = false; //see console for debug log.
 db.run("jquery.js", function(){
-    db.run("editor.js");
+    //db.run("editor.js"); //general-purpose editor.
 	db.run("stationary-editor.js");
     db.run("iframe-controller.js");
 });
